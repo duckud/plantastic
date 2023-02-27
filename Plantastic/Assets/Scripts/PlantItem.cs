e5ad24600cccb1e43a2f70e38c60187ef8bb5a06
Elisa Petit-Sirigu <E.Petit.inf21@stud.bbbaden.ch>
2023-02-28 00:58:36 +0100

    finished corn buy function

--- Plantastic/Assets/Scripts/PlantItem.cs
+++ Plantastic/Assets/Scripts/PlantItem.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlantItem : MonoBehaviour
 {
+    public PlantObject plant;
+
+    public Text nameTxt;
+    public Text priceTxt;
+    public Image icon;
+
+    public Image btnImage;
+    public Text btnTxt;
+
+    FarmManager fm;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        fm = FindObjectOfType<FarmManager>();
+        InitializeUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BuyPlant()
     {
-        
+        Debug.Log("Bought " + plant.plantName);
+        fm.SelectPlant(this);
     }
+
+    void InitializeUI()
+    {
+        nameTxt.text = plant.plantName;
+        priceTxt.text = "$" + plant.buyPrice;
+        icon.sprite = plant.icon;
+    }
+
 }
